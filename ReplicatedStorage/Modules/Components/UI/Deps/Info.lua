6853edfd253f08d943c2d1669ac654198c7454b0
Senko <68160877+Senko3141@users.noreply.github.com>
2021-02-21 08:20:14 -0500

    Create Info.lua

---
+++ ReplicatedStorage/Modules/Components/UI/Deps/Info.lua
@@ -0,0 +1,21 @@
+-- Info
+
+return {
+	["Candy"] = {
+		[1] = {
+			["Desc"] = "Are you here to pick up a quest?",
+			["Responses"] = {
+				[1] = "Yeah, I am.",
+				[2] = "Nah, just looking around."
+			},
+		},
+		
+		[2] = {
+			["Desc"] = "Okay, here's one for you. There's a threat at %s.",
+			["Responses"] = {
+				[1] = "Thanks!",
+				[2] = "Ugh, too hard."
+			},
+		},
+	},
+};
